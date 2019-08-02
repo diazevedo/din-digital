104deadedbd665a00ecbf5d5de7a881f267aae50
Diego Azevedo <dapptn@hotmail.com>
2019-08-02 22:41:51 +1000

    Saving minor changes

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -10,7 +10,7 @@ const Carousel = () => {
   return(
     <div className="carousel-wrapper">
       <ul>
-        <li>
+        <li className="carousel-item" >
 
           <img src={Monitor} alt=""/>
           
@@ -20,6 +20,8 @@ const Carousel = () => {
             </h2>
             <p className="paragraph-box">Cursuspenatisaccum ut curabitur nulla tellus tor ames a in curabitur pede. Idet mollisi eros dis orci congue elis et curabitur consequam intesque. Curabiturpisametur in ante.</p>
             <Button content= "read more »" className= ""/>
+            <a href="#" className="bullet"></a>
+            <a href="#" className="bullet -active"></a>
           </div>
         </li>
       </ul>
