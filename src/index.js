5ef5d15250cca6363f86d2c8b9af37d9b99d7954
Diego Azevedo <dapptn@hotmail.com>
2019-08-02 14:18:34 +1000

    Reset css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '../src/styles/generic/reset.css'
 import './index.css';
+
+
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
